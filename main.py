43a0a3defec3d0d53a8fb9fa8dfe5b318e6665e8
fasthill <kangeun@naver.com>
2021-04-18 20:58:15 +0900

    main changed

--- main.py
+++ main.py
@@ -21,19 +21,5 @@ if __name__ == '__main__':
     print(add_numbers(4, 3))
     alex_method("ali")
 
-# This is a sample Python script.
+import this
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
